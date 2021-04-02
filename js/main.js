4aa45ff78971103d47bd7903be5c01f09d3ef86d
Miloschool <19077165@student.hhs.nl>
2021-04-02 16:32:38 +0200

    fuck weather api shit

--- js/main.js
+++ js/main.js
@@ -5,32 +5,67 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWlpbG8wdyIsImEiOiJja21heGhuczgxdzZpMnlrbmJla
 var map = new mapboxgl.Map({
 container: 'map', // container ID
 style: 'mapbox://styles/mapbox/streets-v11', // style URL
-center: [-74.5, 40], // starting position [lng, lat]
+center: [4.288788, 52.078663], // starting position [lng, lat]
 zoom: 9 // starting zoom
 });
 
-var geoLocation = new MapboxGeocoder({ // Create to be able to access my token
+var coorLat = 52.078663;
+var coorLon = 4.288788;
+
+
+
+var geoLocation = new MapboxGeocoder({ // toegang tot mijn token
     accessToken: mapboxgl.accessToken, 
     mapboxgl: mapboxgl,
-})
 
-var locationName = document.getElementById('geocoder').appendChild(geoLocation.onAdd(map)); //searchbar
-
-
-var suggestionsShown = document.getElementsByClassName('suggestions');
-
-suggestionsShown.addEventListener('click', function(e){
-
-    console.log('WORKS LAN');
+    getItemValue: e =>{
+        coorLon = e['center'][0];
+        coorLat = e['center'][1];
+          document.getElementsByClassName('mapboxgl-ctrl-geocoder--input').value = e['place_name']; //lat
+          return e['place_name'];
+        }
+   
 });
 
-// setInterval(function(){ 
-//     // var getLocationName = document.getElementsByClassName('mapboxgl-ctrl-geocoder--input')[0].value;
-//     // document.getElementById('locationNameOutput').innerHTML = getLocationName;
 
 
 
 
-// }, 500);
 
+var locationName = document.getElementById('geocoder').appendChild(geoLocation.onAdd(map)); //toevoegen van zoekbalk
+
+setInterval(function(){ 
+
+    
+    var getLocationName = document.getElementsByClassName('mapboxgl-ctrl-geocoder--input')[0].value; // waarde vanuit de zoekbalk
+    document.getElementById('locationNameOutput').innerHTML = getLocationName;// locatie naam latenzien
+
+      //weather
+
+      const key = 'd25be8eef3d94997a5b8d5df3bb7e8b3'; //Key voor API
+      var temp = document.getElementById('showWeatherTemp');
+      var weatherIcon = document.getElementById('weatherIcon');
+      
+      fetch('https://api.weatherbit.io/v2.0/current?lat=' + coorLat + '&lon=' + coorLon +'&key=' + key+ '&include=minutely') // Haalt data van openweather API
+          .then(res => res.json())
+          .then(data => {
+   
+   
+             temp.innerHTML = data['data'][0].temp + ' °C';
+        
+   
+          
+              })
+          .catch(err => console.log('Fill city name!'))
+              
+
+}, 2000);
+
+
+
+
+
+
+
+// weather
 
